906f184bb58963adda0b0c1d606af9436c136bb9
Michiel van Liempt <michiel.v.liempt@gmail.com>
2016-01-06 21:04:09 +0100

    work on android plugin

--- native/android/pay/example/src/main/java/io/vigour/plugin/example/MainActivity.java
+++ native/android/pay/example/src/main/java/io/vigour/plugin/example/MainActivity.java
@@ -1,65 +1,49 @@
 package io.vigour.plugin.example;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import io.vigour.plugin.pay.PayPlugin;
 import io.vigour.plugin.statusbar.MapWrapper;
 import io.vigour.plugin.statusbar.StatusBarPlugin;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
-    StatusBarPlugin plugin;
+    PayPlugin plugin;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        plugin = new StatusBarPlugin(this, findViewById(R.id.screen));
+        plugin = new PayPlugin(this);
+        textView = (TextView) findViewById(R.id.screen);
     }
 
-    public void hidden(View v) {
+    public void getProducts(View v) {
+        textView.setText("clicked getProducts");
+        textView.setText(plugin.getProducts("{}"));
     }
 
-    public void over(View v) {
+    public void buySingle(View v) {
+        textView.setText("clicked buySingle");
+        textView.setText(plugin.buy("single"));
     }
 
-    public void top(View v) {
+    public void buyMonthly(View v) {
+        textView.setText("clicked buyMonthly");
+        textView.setText(plugin.buy("monthly"));
     }
 
-    public void black(View v) {
-    }
-
-    public void white(View v) {
-    }
-
-    public void purple(View v) {
-    }
-
-    public void lime(View v) {
-    }
-
-    public void alpha0(View v) {
-    }
-
-    public void alpha1(View v) {
-    }
-
-    public void alpha04(View v) {
-    }
-
-    private Map<String, Object> makeMap(String... keyvals) {
-        if (keyvals.length % 2 != 0) {
-            throw new IllegalArgumentException("must have even amount of args");
-        }
-        HashMap<String, Object> map = new HashMap<>();
-        for (int i = 0; i < keyvals.length; i+=2) {
-            map.put(keyvals[i], keyvals[i+1]);
-        }
-        return map;
+    public void buyYearly(View v) {
+        textView.setText("clicked buyYearly");
+        textView.setText(plugin.buy("yearly"));
     }
 }
---
+++ native/android/pay/plugin-pay/src/main/java/io/vigour/plugin/pay/PayPlugin.java
@@ -0,0 +1,139 @@
+package io.vigour.plugin.pay;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Color;
+import android.os.Build;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.fasterxml.jackson.jr.ob.JSON;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+import io.vigour.nativewrapper.plugin.core.Plugin;
+import io.vigour.plugin.pay.util.IabHelper;
+import io.vigour.plugin.pay.util.IabResult;
+
+/**
+ * Created by michielvanliempt on 09/04/15.
+ */
+public class PayPlugin extends Plugin {
+
+    private static final String TAG = "pay";
+    private final IabHelper helper;
+    public static final int PURCHASE_REQUEST_CODE = 1;
+
+    public PayPlugin(Context context) {
+        super("pay");
+
+
+        String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAk/28zyBFmP0Wexr32L/X3iQvSvMVs4M3DKE4kZunjKYnpl8uwYuNSxs4Alog430LQ70j9SfQho5ptk0yPcArgthUJXETsqZYoWMF7lLRDtEg9y1Dc13Fv2iqcWbJN0cULg65ZZbJQ+H0xRE6ttk9oehVl7GbO96T9DtAkJZYa63kwbxLLAdSYSmFMo2Pr7sBuXzR2LEKjfqXzLKX6JL2y891csaVzJmn2kh4RQuwwnJFVQtv+2k6hKnm7RI2D7tq6bDOgtUDDOmk3ZaF03iToEObq+ccvVoGOmacyUpsI+YnAlt44fRFjG3WVYaBmhd/CZbpIrZqt3PVR9PDZnVYOwIDAQAB"; // mtv packer
+
+        // compute your public key and store it in base64EncodedPublicKey
+        helper = new IabHelper(context, base64EncodedPublicKey);
+        helper.enableDebugLogging(true, TAG);
+        helper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+            public void onIabSetupFinished(IabResult result) {
+                if (!result.isSuccess()) {
+                    // Oh noes, there was a problem.
+                    Log.e(TAG, "Problem setting up In-app Billing: " + result);
+                }
+                Log.i(TAG, "setting up In-app Billing succesful: " + result);
+            }
+        });
+
+    }
+
+    public String getProducts(String info) {
+        return "{\n" +
+               "    \"testRegion_single_test\": {\n" +
+               "      \"price\": 0.99\n" +
+               "    },\n" +
+               "    \"testRegion_monthly_test\": {\n" +
+               "      \"price\": 4.99\n" +
+               "    },\n" +
+               "    \"testRegion_yearly_test\": {\n" +
+               "      \"price\": 14.99\n" +
+               "    }\n" +
+               "  }";
+    }
+
+    public String buy(String productId) {
+        return productId;
+    }
+
+    /*
+    public void onPurchaseResult(int resultCode, Intent data) {
+        if (!helper.handleActivityResult(PURCHASE_REQUEST_CODE, resultCode, data)) {
+            Log.d(TAG, "onActivityResult not handled by IABUtil.");
+        }
+        else {
+            Log.d(TAG, "onActivityResult handled by IABUtil.");
+        }
+    }
+
+    public void onDestroy() {
+        if (helper != null) helper.dispose();
+    }
+
+    public String listItems() {
+        List<String> skus = new ArrayList<>();
+        //get skus from strings or some other xml / json
+        if (mHelper.isAsyncInProgress()) {
+            debug("can't refresh: async in progress");
+            return;
+        }
+        debug("refreshing...");
+        mHelper.queryInventoryAsync(true, skus, new IabHelper.QueryInventoryFinishedListener() {
+            @Override
+            public void onQueryInventoryFinished(IabResult result, Inventory inv) {
+                if (result.isFailure()) {
+                    toast("loading items failed");
+                    return;
+                }
+
+                toast(String.format("got items from store"));
+//                output.setText(inv.toDebugString());
+                List<SkuDetails> detailsList = new ArrayList<>();
+                List<Purchase> haveList = new ArrayList<>();
+                for (Items item : Items.values()) {
+                    detailsList.add(inv.getSkuDetails(item.name));
+                    Purchase purchase = inv.getPurchase(item.name);
+                    if (purchase != null) {
+                        haveList.add(purchase);
+                    }
+                }
+                populateStoreList(detailsList);
+                populateHaveList(haveList);
+                debug("refresh done");
+            }
+        });
+    }
+
+    public void buyItem(SkuDetails details) {
+        String toast = String.format("buying %s %s...", details.getType(), details.getSku());
+        toast(toast);
+        if (mHelper.isAsyncInProgress()) {
+            debug("can't buy: async in progress...");
+            return;
+        }
+        debug("buying...");
+        mHelper.launchPurchaseFlow(this, details.getSku(), details.getType(), PURCHASE_REQUEST_CODE, new IabHelper.OnIabPurchaseFinishedListener() {
+            @Override
+            public void onIabPurchaseFinished(IabResult result, Purchase info) {
+                onResult(result);
+            }
+        }, null);
+    }
+
+
+    */
+}
